f54cf69c21e134d8988226cc3fbd8c1f3fb7bed7
mmmatija <m.martinovic100@gmail.com>
2025-01-25 14:20:09 +0100

    nove eksplozije -> dvije vrste
    +vracen zvuk na eksplozije

--- iSS/Assets/Grupa1/Scripts/rocket.cs
+++ iSS/Assets/Grupa1/Scripts/rocket.cs
@@ -7,21 +7,41 @@ public class Rocket : MonoBehaviour
 
     private Transform cameraTransform;
     private Rigidbody rb;
+    private Vector3 spawnPoint;
     [SerializeField] private float movementSpeed = 25;
     [SerializeField] private float rotateSpeed = 5;
-    [SerializeField] private ParticleSystem explosionPrefab;
+    [SerializeField] private ParticleSystem tankExplosionPrefab;
+    [SerializeField] private ParticleSystem terrainExplosionPrefab;
+    [SerializeField] private ParticleSystem waterExplosionPrefab;
 
     private void Start()
     {
+        spawnPoint = transform.position;
         rb = GetComponent<Rigidbody>();
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Player"))
         {
-            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            Destroy(gameObject);
+            if (collision.gameObject.CompareTag("Tank"))
+            {
+                Instantiate(tankExplosionPrefab, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            else if (collision.gameObject.CompareTag("Water"))
+            {
+                rb.velocity = Vector3.zero;
+                StartCoroutine(ExplodeAfterDelay(2f));
+                //Instantiate(waterExplosionPrefab, transform.position, Quaternion.identity);
+                //Destroy(gameObject);
+            }
+            else
+            {
+                Instantiate(terrainExplosionPrefab, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
         }
+       
     }
 
     private void FixedUpdate()
@@ -30,6 +50,11 @@ public class Rocket : MonoBehaviour
         rb.velocity = transform.forward * movementSpeed;
 
         RotateMissile();
+
+        if(Vector3.Distance(transform.position, spawnPoint) > 1500f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void RotateMissile()
@@ -44,5 +69,12 @@ public class Rocket : MonoBehaviour
     public void SetCameraTransform(Transform camera)
     {
         cameraTransform = camera;
+    }
+
+    private IEnumerator ExplodeAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // Wait for the specified delay
+        Instantiate(waterExplosionPrefab, transform.position, Quaternion.identity); // Create the water explosion
+        Destroy(gameObject); // Destroy the missile
     }
 }
